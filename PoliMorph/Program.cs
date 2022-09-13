a80326020af90d091ac6ff514e245aaef5a761d5
Lebedev Timofey <lebedev.timofey007@gmail.com>
2022-09-13 17:04:20 +0300

    Finished lesson5

---
+++ PoliMorph/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PoliMorph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new RationalNumbers(2, 4);
+            var b = new RationalNumbers(2, 4);
+            
+            if ( a == b)
+            {
+                Console.WriteLine("im happy");
+            }
+
+           
+        }
+    }
+}
